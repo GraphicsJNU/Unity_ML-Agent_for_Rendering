df39c32a5b85b6e2b2684638848b4117e3a243a0
namunamunamu <kencoor9@gmail.com>
2023-08-02 17:16:15 +0900

    ViewSynthesis 학습 코드 및 config 추가

--- Assets/Scripts/ViewSynthesisAgent.cs
+++ Assets/Scripts/ViewSynthesisAgent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
@@ -7,58 +8,136 @@ using Unity.MLAgents.Actuators;
 
 public class ViewSynthesisAgent : Agent
 {
+    public int _multiplier;
+    public Transform AgentCamera;
+    public Transform TargetCamera;
+    public RenderTexture TargetRT;
+    public RenderTexture AgentRT;
+    public Transform TargetFocalPoint;
+    public Transform AgentFocalPoint;
+
+
+    private Texture2D TargetTex;
+    private Texture2D AgentTex;
+    private float loss;
+    private float _threshold;
+    private int iter;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetTexture("Answer_tex");
+        int texhight = TargetRT.height;
+        int texwidth = TargetRT.width;
+        TargetTex = new Texture2D(texhight, texwidth, TextureFormat.RGB24, false);
+        AgentTex = new Texture2D(texhight, texwidth, TextureFormat.RGB24, false);
+        loss = 0;
+        _threshold = 0.9f;
+        iter = 0;
     }
 
     public override void OnEpisodeBegin()
     {
-        this.GetChild(3).localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
-        this.GetChild(3).localRotation = Quaternion.Euler(0, Random.value * 360, 0);
+        AgentCamera.localPosition = new Vector3(0, 0.5f, 0);
+        AgentCamera.LookAt(AgentFocalPoint);
+
+        TargetCamera.localPosition = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(0.5f, 10.0f), Random.Range(-10.0f, 10.0f));
+        TargetCamera.LookAt(TargetFocalPoint);
+
+        SetTexture(TargetTex, TargetRT);
+        SetTexture(AgentTex, AgentRT);
+        iter++;
     }
 
-    public override void CollectObservations(VectorSensor sensor){
-        sensor.AddObservation(this.GetChild(3).localPosition);
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        loss = GetPixelLoss(TargetTex, AgentTex);
+        //Debug.Log("Loss = "+loss);
+        sensor.AddObservation(loss);
+        sensor.AddObservation(AgentCamera.localPosition);
+        sensor.AddObservation(AgentCamera.rotation);
+        
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers){
+        //AddReward(-0.001f);
+
         Vector3 positioncontrol = Vector3.zero;
-        Vector3 rotationcontrol = Vector3.zero;
         positioncontrol.x = actionBuffers.ContinuousActions[0];
-        positioncontrol.z = actionBuffers.ContinuousActions[2];
         positioncontrol.y = actionBuffers.ContinuousActions[1];
-        rotationcontrol.x = actionBuffers.ContinuousActions[3];
-        rotationcontrol.y = actionBuffers.ContinuousActions[4];
-        rotationcontrol.z = actionBuffers.ContinuousActions[5];
+        positioncontrol.z = actionBuffers.ContinuousActions[2];
 
-        this.GetChild(3).localPosition += positioncontrol;
-        this.GetChild(3).localRotation *= Quaternion.Euler(rotationcontrol);
+         AgentCamera.Translate(positioncontrol*_multiplier);
+        //AgentCamera.localPosition = positioncontrol*_multiplier;
+        AgentCamera.LookAt(AgentFocalPoint);
+        SetTexture(AgentTex, AgentRT);
 
-        float pixelLoss = 1;//픽셀로스 구현해야합니다.
-
-        if(pixelLoss < 0.1f){
-            SetReward(1.0f);
+        if(AgentCamera.position.y < 0 
+        || AgentCamera.position.y > 31 
+        || AgentCamera.position.x > 31 
+        || AgentCamera.position.x < -31 
+        || AgentCamera.position.z > 31 
+        || AgentCamera.position.z < -31 ){
+            AddReward(-10.0f);
             EndEpisode();
-        }
-        if(this.GetChild(3).localPosition.y < 0 || this.GetChild(3).localPosition.y > 30 || this.GetChild(3).localPosition.x > 30 || this.GetChild(3).localPosition.x < -30 || this.GetChild(3).localPosition.z > 30 || this.GetChild(3).localPosition.z < -30 ){
-            EndEpisode();
-        }
+        } 
 
+        loss = GetPixelLoss(TargetTex, AgentTex);
+        RewardTime(loss, TargetTex.height*TargetTex.width, _threshold);
     }
 
-    private Texture2D GetTexture(string filename)
-    {
-        RenderTexture rt = (RenderTexture)Resources.Load("Textures/" + filename);
-        Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+    // public override void Heuristic(in ActionBuffers actionsOut){
+    //     ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+    //     continuousActions[0] = Input.GetAxisRaw("Horizontal");
+    //     continuousActions[1] = Input.GetKey(KeyCode.Space) ? 1.0f : 0.0f;
+    //     continuousActions[2] = Input.GetAxisRaw("Vertical");
 
+    // }
+
+    private void SetTexture(Texture2D tex, RenderTexture rt)
+    {
         // ReadPixels looks at the active RenderTexture.
         RenderTexture.active = rt;
         tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
         tex.Apply();
-        Debug.Log(tex.GetPixel(0, 0));
+    }
 
-        return tex;
+    private float GetPixelLoss(Texture2D tex1, Texture2D tex2)
+    {
+        int height = tex1.height;
+        int width = tex1.width;
+        float onepixelloss = 0;
+        float meanpixelloss = 0;
+
+        Color[] pixels1 = tex1.GetPixels();
+        Color[] pixels2 = tex2.GetPixels();
+
+        for(int i = 0; i<pixels1.Length; i++){
+            onepixelloss = (pixels1[i].r - pixels2[i].r) + (pixels1[i].g - pixels2[i].g) + (pixels1[i].b - pixels2[i].b);
+            onepixelloss = onepixelloss*onepixelloss;
+            meanpixelloss += (onepixelloss/3);
+        }
+        meanpixelloss /= pixels1.Length;
+        return meanpixelloss;
+    }
+
+    private void RewardTime(float loss, int pixels_num, float threshold = 0.9f){
+        if(loss < 0.1f) {
+            SetReward(0.01f);
+            //Debug.Log(iter + " : Not bad!");
+        }
+        if(loss < 0.05f) {
+            SetReward(0.05f);
+            //Debug.Log(iter + " : Good!");
+        }
+        if(loss < 0.01f){
+            SetReward(0.1f);
+            Debug.Log(iter + " : Almost!");
+        }
+        if(loss < 0.001f) {
+            SetReward(10.0f);
+            Debug.Log(iter + " : Correct!");
+            //Thread.Sleep(5000); //학습결과 확인용
+            EndEpisode();
+        }
     }
 }
